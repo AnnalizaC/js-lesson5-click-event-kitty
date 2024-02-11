f4f15e323de90c0e8c42998f9557490912dec803
Annaliza_C <aldcadiente@gmail.com>
2024-02-10 22:26:22 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+// console.log(button);
+// console.log(cat);
+
+button.addEventListener("click", function() {
+    // console.log("It's working");
+    cat.classList.add("show");
+});
